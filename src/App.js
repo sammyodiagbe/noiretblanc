9ab29ed410fcd1ca44b65f114daa731dc4d834a5
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-29 21:27:53 -0500

    working on app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import FacebookIcon from "./componentss/facebook";
+import InstagramIcon from "./componentss/instagram";
+import TwitterIcon from "./componentss/twiitter";
+import { useState } from "react";
+import image from "./assets/image.png";
+import yemiImage from "./assets/Headshot.JPG";
+import Nav from "./componentss/nav";
+import "./App.css";
 
 function App() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"container"}>
+      <Nav />
+      <main className="main-content">
+        <section className="introduction">
+          <div className="left">
+            <h1 className="title">
+              Professional Accounting and Bookeeping Service for your business
+            </h1>
+            <p>
+              Our team of highly Skilled accounting and financial professional
+              are available to help you grow your business by providing
+              high-quality bookeeping and accounting services.
+            </p>
+            <a href="#contact">Send a message</a>
+          </div>
+          <div className="right">
+            <img src={image} alt="Man giving accounting advice to client" />
+          </div>
+        </section>
+        <section className="offers">
+          <h2 className="title">What we offer.</h2>
+          <div className="offers">
+            <div className="offer">
+              <h2 className="number">01</h2>
+              <div className="details">
+                <h3>Accounting System Setup</h3>
+                <p>
+                  Business process automation for small and large businesses.
+                </p>
+              </div>
+            </div>
+            <div className="offer">
+              <h2 className="number">02</h2>
+              <div className="details">
+                <h3>Business Process Improvement</h3>
+                <p>
+                  Advice and Research on how to save costs and grow your
+                  business.
+                </p>
+              </div>
+            </div>
+            <div className="offer">
+              <h2 className="number">03</h2>
+              <div className="details">
+                <h3>Bookkeeping and Accounting</h3>
+                <p>
+                  Accounts Receivable / Accounts Payable, Regular monthly
+                  enteries, Reconciliation of Bank and Credit card statements ,
+                  Year-end close.
+                </p>
+              </div>
+            </div>
+            <div className="offer">
+              <h2 className="number">04</h2>
+              <div className="details">
+                <h3>Payroll</h3>
+                <p>Payroll Processing</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="meet-yemi">
+          <div className="img-container">
+            <img
+              src={yemiImage}
+              alt="Yemi is a chartered professional accountant"
+            />
+          </div>
+          <div className="details">
+            <h2 className="title">Meet Yemi.</h2>
+            <p>
+              Yemi Olaleye is a Chartered Professional Accountant (CPA) with
+              over 16 years of experience in accounting and finance. Yemi is an
+              alumnus of the University of Liverpool, where he obtained his
+              Master’s degree in International Accounting and Finance Yemi’s
+              passion is to help businesses and individuals make better
+              financial decisions by providing relevant financial and business
+              advice
+            </p>
+          </div>
+        </section>
+        <section className="contact">
+          <div className="left">
+            <h2 className="title">We would love to hear from you?</h2>
+            <p>
+              To help us best serve your inquiry, we recommend that you first
+              describe the issue you’re having before telling us what you want
+              to achieve. You may also email or call us to make an appointment.
+              Our general response time is one business day.
+            </p>
+          </div>
+          <div className="right">
+            <form className="contact-form">
+              <div className="names-container">
+                <div className="input-container">
+                  <input
+                    type="text"
+                    placeholder="Firstname"
+                    value={firstname}
+                    onChange={({ target }) => setFirstname(target.value)}
+                  />
+                </div>
+                <div className="input-container">
+                  <input
+                    type="text"
+                    placeholder="Lastname"
+                    value={lastname}
+                    onChange={({ target }) => setLastname(target.value)}
+                  />
+                </div>
+              </div>
+              <div className="input-container">
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={({ target }) => setEmail(target.value)}
+                />
+              </div>
+              <div className="input-container">
+                <textarea
+                  value={message}
+                  placeholder="Your message..."
+                  onChange={({ target }) => setMessage(target.value)}
+                ></textarea>
+              </div>
+              <div className="input-container">
+                <button className="btn">Send message</button>
+              </div>
+            </form>
+          </div>
+        </section>
+      </main>
+      <footer className="footer">
+        <h2 className="logo">Noiretblanc</h2>
+        <ul className="nav">
+          <li>
+            <a href="/contact-us">
+              <FacebookIcon />
+            </a>
+          </li>
+          <li>
+            <a href="/our-team">
+              <InstagramIcon />
+            </a>
+          </li>
+          <li>
+            <a href="/our-team">
+              <TwitterIcon />
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
---
+++ src/componentss/nav.js
@@ -0,0 +1,38 @@
+import BurgerMenu from "./burgerMenu";
+import { useState } from "react";
+
+const Nav = () => {
+  const [showmenu, setShowMenu] = useState(false);
+  return (
+    <nav className="navigation">
+      <h2 className="logo">Noiretblanc</h2>
+      <button
+        onClick={() => setShowMenu(true)}
+        style={{ opacity: showmenu && "0.3" }}
+      >
+        <BurgerMenu />
+      </button>
+      <div className={`subnav ${showmenu ? "show-menu" : ""}`}>
+        <button className="close-btn" onClick={() => setShowMenu(false)}>
+          &times;
+        </button>
+        <ul className="nav">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact-us">Contact us</a>
+          </li>
+          <li>
+            <a href="/our-team">Our team</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
